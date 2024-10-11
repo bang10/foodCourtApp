4f1b51f44f66a6f12b4ec45e8716709961db1549
bangseonghwan <bang369953@gmail.com>
2024-10-11 16:07:12 +0900

    Initial Commit

---
+++ foodCourtApp/foodCourtAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodCourtAppApp.swift
+//  foodCourtApp
+//
+//  Created by bangseonghwan on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct foodCourtAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ foodCourtAppTests/foodCourtAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  foodCourtAppTests.swift
+//  foodCourtAppTests
+//
+//  Created by bangseonghwan on 10/11/24.
+//
+
+import Testing
+
+struct foodCourtAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
